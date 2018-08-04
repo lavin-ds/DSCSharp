031a6c860a4336ec082edb6384bb4efae4b764ac
Lavin Sharma <lifeslong@gmail.com>
2018-08-04 19:20:21 +0100

    DoublyLL entity

---
+++ DSC/Entities/DoublyLinkedList.cs
@@ -0,0 +1,22 @@
+/***********************************************************
+Author          - lavinds
+Date Created    - 04-Aug-2018
+************************************************************
+Sn      Author      Date            Comments
+************************************************************
+1.      lavinds     04-Aug-2018     Initial draft of file
+************************************************************/
+
+#region Namespaces
+using System;
+#endregion
+
+namespace DataStructuresCsharp
+{       
+    public class DoublyLinkedListNode
+    {
+        public int data;
+        public LinkedListNode next;
+        public LinkedListNode previous;
+    }
+}
