46986544cbcbe40ca0ca7c0bf80838826d4e803a
Lavin Sharma <lifeslong@gmail.com>
2018-08-05 17:56:57 +0100

    Introduced BinaryTreeNode

---
+++ DSC/Entities/BinaryTreeNode.cs
@@ -0,0 +1,22 @@
+/***********************************************************
+Author          - lavinds
+Date Created    - 05-Aug-2018
+************************************************************
+Sn      Author      Date            Comments
+************************************************************
+1.      lavinds     05-Aug-2018     Initial draft of file
+************************************************************/
+
+#region Namespaces
+using System;
+#endregion
+
+namespace DataStructuresCsharp
+{       
+    public class BinaryTreeNode
+    {
+        public int data;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+    }
+}
