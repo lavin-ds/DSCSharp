0f64604622323189bf01c7709214483af9e223e3
Lavin Sharma <lifeslong@gmail.com>
2018-08-06 23:45:01 +0100

    Need to fix list issues

---
+++ DSC/ImplementationClasses/BinaryTree.cs
@@ -0,0 +1,52 @@
+/***********************************************************
+Author          - lavinds
+Date Created    - 05-Aug-2018
+Description     - Class that contains the methods for 
+                  LinkedLists
+************************************************************
+Sn      Author      Date            Comments
+************************************************************
+1.      lavinds     05-Aug-2018     Initial draft of file
+************************************************************/
+
+#region Namespaces
+using System; 
+#endregion
+
+namespace DataStructuresCsharp
+{
+    public class BinaryTree
+    {
+        private BinaryTreeNode root;
+
+        private BinaryTreeNode head;
+
+        List<BinaryTreeNode> resultListPreOrderRecursive = new List<BinaryTreeNode>();
+
+        public void TraverseListPreOrderRecursive(BinaryTreeNode root)
+        {            
+            if(root)
+            {
+                resultListPreOrderRecursive.Add(root.data);
+                TraverseListPreOrderRecursive(root.left);
+                TraverseListPreOrderRecursive(root.right);
+            }
+        }
+
+        // public void InsertElement(int data)
+        // {
+        //     if(root == null)
+        //     {
+        //         root = new BinaryTreeNode();
+        //         root.data = data;
+        //         root.left = null;
+        //         root.right = null; 
+        //         head = root;
+        //     }
+        //     else 
+        //     {
+        //         if()         
+        //     }
+        // }
+    }
+}
