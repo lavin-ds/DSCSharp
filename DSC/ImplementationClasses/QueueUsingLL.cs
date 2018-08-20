3c3ac5622be17d1f5d980a87c21c9f1bbef1a2b5
Lavin <lifeslong@gmail.com>
2018-08-20 09:56:31 +0100

    Initial Queue draft

---
+++ DSC/ImplementationClasses/QueueUsingLL.cs
@@ -0,0 +1,25 @@
+/***********************************************************
+Author          - lavinds
+Date Created    - 20-Aug-2018
+Description     - Class that contains the methods for Queue
+                  using LinkedLists as the data structure 
+                  for storage
+************************************************************
+Sn      Author      Date            Comments
+************************************************************
+1.      lavinds     20-Aug-2018     Initial draft of file
+************************************************************/
+
+#region Namespaces
+using System;
+#endregion
+
+namespace DataStructuresCsharp
+{       
+    public class Queue<T>
+    {    
+        private LinkedList<T> top = new LinkedList<T>();  
+        private int max = -1;
+        private int sum = 0;
+    }
+}
