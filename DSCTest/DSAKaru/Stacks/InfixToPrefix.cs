e44c4c410085c57dcd6ed8fb74b6c6d915dfb9f4
lds <lifeslong@gmail.com>
2021-03-23 22:17:29 +0100

    Setup Infix to Prefix

---
+++ DSCTest/DSAKaru/Stacks/InfixToPrefix.cs
@@ -0,0 +1,15 @@
+using DataStructuresCsharp;
+
+namespace DSCTest
+{
+    public class InfixToPrefix
+    {
+        public string ConvertInfixToPrefix(string input)
+        {
+            StackUsingLL<int> staging = new StackUsingLL<int>();
+            StringBuilder result = new StringBuilder();
+            
+             
+        }
+    }
+}
