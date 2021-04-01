73a0a8fd388305f329b5a7b909d580918c5f8d4d
lds <lifeslong@gmail.com>
2021-04-01 22:05:40 +0200

    Incorrect MaximalRank added

--- Algorithms/LeetCode/Graphs/1615_MaximalRank.cs
+++ Algorithms/LeetCode/Graphs/1615_MaximalRank.cs
@@ -1,6 +1,8 @@
 
 using System;
 /*
+TODO:Incorrect
+
 There is an infrastructure of n cities with some number of roads connecting these cities. Each roads[i] = [ai, bi] indicates that there is a bidirectional road between cities ai and bi.
 
 The network rank of two different cities is defined as the total number of directly connected roads to either city. If a road is directly connected to both cities, it is only counted once.
