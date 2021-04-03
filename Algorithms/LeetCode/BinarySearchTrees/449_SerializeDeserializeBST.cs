9fce608012ad7748b705d0eea6a8074eaf12e89b
lds <lifeslong@gmail.com>
2021-04-03 16:38:45 +0200

    Added serialize deserializeBST

--- Algorithms/LeetCode/BinarySearchTrees/449_SerializeDeserializeBST.cs
+++ Algorithms/LeetCode/BinarySearchTrees/449_SerializeDeserializeBST.cs
@@ -1,45 +1,37 @@
+using System;
+using System.Collections;
 /*
-297. Serialize and Deserialize Binary Tree
-https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
-Hard
+449. Serialize and Deserialize BST
+https://leetcode.com/problems/serialize-and-deserialize-bst/
+Medium
 
-Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+Serialization is converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
 
-Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You need to ensure that a binary search tree can be serialized to a string, and this string can be deserialized to the original tree structure.
 
-Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+The encoded string should be as compact as possible.
 
- Example 1:
+Example 1:
 
-Input: root = [1,2,3,null,null,4,5]
-Output: [1,2,3,null,null,4,5]
+Input: root = [2,1,3]
+Output: [2,1,3]
 
 Example 2:
 
 Input: root = []
 Output: []
 
-Example 3:
-
-Input: root = [1]
-Output: [1]
-
-Example 4:
-
-Input: root = [1,2]
-Output: [1,2]
-
 Constraints:
 
     The number of nodes in the tree is in the range [0, 104].
-    -1000 <= Node.val <= 1000
+    0 <= Node.val <= 104
+    The input tree is guaranteed to be a binary search tree.
 */
 
 using System.Text;
 using Xunit;
-using System;
 
-namespace Algorithms.LeetCode.Trees
+namespace Algorithms.LeetCode.BinarySearchTrees
 {
     public class TreeNode 
     {
@@ -117,6 +109,6 @@ namespace Algorithms.LeetCode.Trees
             var res = Serialize(result);
             Assert.Equal("1,2,X,X,3,4,X,X,5,X,X",res);
             result = Deserialize(res);
-        }
+       }
     }
 }
