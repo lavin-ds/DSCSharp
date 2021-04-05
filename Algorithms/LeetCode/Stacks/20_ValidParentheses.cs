dc647dd81265ca8dfa76b899e76f67fb469d57e5
lds <lifeslong@gmail.com>
2021-04-05 21:44:38 +0200

    Add valid parentheses

---
+++ Algorithms/LeetCode/Stacks/20_ValidParentheses.cs
@@ -0,0 +1,95 @@
+using System.Reflection.Metadata.Ecma335;
+/*
+20. Valid Parentheses
+https://leetcode.com/problems/valid-parentheses/
+Easy
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+Example 4:
+
+Input: s = "([)]"
+Output: false
+
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+
+
+ */
+
+using Xunit;
+using System.Collections;
+using System;
+
+namespace Algorithms.LeetCode.Stacks
+{
+    public class ValidParentheses 
+    {
+         public bool IsValid(string s) 
+        {
+            Stack stage = new Stack();
+            for(int i = 0; i<s.Length; i++)
+            {    
+
+                if(s[i] ==  '(' || s[i] == '{'  || s[i] == '[')
+                {
+                    stage.Push(s[i]);
+                }
+                else
+                {
+                    switch(s[i])
+                    {
+                        case ')' :
+                            if(Convert.ToChar(stage.Peek()) == '(')
+                                stage.Pop();
+                            else 
+                                return false;                        
+                            break;
+                        case ']' :
+                            if(Convert.ToChar(stage.Peek()) == ']')
+                                stage.Pop();
+                            else 
+                                return false;
+                            break;
+                        case '}' :
+                            if(Convert.ToChar(stage.Peek()) == '}')
+                                stage.Pop();
+                            else 
+                                return false;
+                            break;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
