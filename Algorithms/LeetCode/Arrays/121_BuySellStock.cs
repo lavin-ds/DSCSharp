2395fa3d071b062970143ac721808c0b238dc103
lds <lifeslong@gmail.com>
2021-04-05 21:30:38 +0200

    Update problem info

--- Algorithms/LeetCode/Arrays/121_BuySellStock.cs
+++ Algorithms/LeetCode/Arrays/121_BuySellStock.cs
@@ -1,5 +1,8 @@
-using System;
 /*
+21. Best Time to Buy and Sell Stock
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Easy
+
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
@@ -26,7 +29,7 @@ Constraints:
  */
 
 using Xunit;
-
+using System;
 namespace Algorithms.LeetCode.Arrays
 {
     public class BuySellStock 
