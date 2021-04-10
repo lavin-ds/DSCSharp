d3c02829e65d9d14a98efd795979921eb27651c1
Lavin S <lifeslong@gmail.com>
2021-04-10 18:23:04 +0200

    Update 1239_MaxLength.cs

--- Algorithms/LeetCode/Strings/1239_MaxLength.cs
+++ Algorithms/LeetCode/Strings/1239_MaxLength.cs
@@ -1,5 +1,5 @@
 /*
-Given an array of strings arr. String s is a concatenation of a sub-sequence of arr which have unique characters.
+1239. Given an array of strings arr. String s is a concatenation of a sub-sequence of arr which have unique characters.
 
 Return the maximum possible length of s.
 
