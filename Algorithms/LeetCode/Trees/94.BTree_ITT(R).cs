ee95e21bea2f6fa3801b6da6167c4d2309be41e7
lds <lifeslong@gmail.com>
2021-04-27 22:57:26 +0200

    Added BTree ITT Recursive

---
+++ Algorithms/LeetCode/Trees/94.BTree_ITT(R).cs
@@ -0,0 +1,89 @@
+/*
+94. Binary Tree Inorder Traversal
+https://leetcode.com/problems/binary-tree-inorder-traversal/ 
+Medium
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+                1
+                 \
+                  2
+                 /
+                3
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+              
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+Example 4:
+                1
+               /
+              2
+                
+Input: root = [1,2]
+Output: [2,1]
+
+Example 5:
+               1
+                \
+                 2
+
+Input: root = [1,null,2]
+Output: [1,2]
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+*/
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.LeetCode.Trees
+{
+    public class BTreeInorderTraversalRecursive
+     { 
+        /**
+        * Definition for a binary tree node.
+        * public class TreeNode {
+        *     public int val;
+        *     public TreeNode left;
+        *     public TreeNode right;
+        *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        *         this.val = val;
+        *         this.left = left;
+        *         this.right = right;
+        *     }
+        * }
+        */
+        
+        private List<int> _lst;
+        
+        public IList<int> InorderTraversal(TreeNode root) {
+            _lst = new List<int>();
+            ITT(root);       
+            return _lst;
+        }
+        
+        public void ITT(TreeNode node)
+        {
+            if(node != null)
+            {
+                ITT(node.left);
+                _lst.Add(node.val);            
+                ITT(node.right);
+            }
+        }
+    }
+}
