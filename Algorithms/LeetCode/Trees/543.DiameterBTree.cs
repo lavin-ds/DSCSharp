dd7123e6485099bc5381df7614d9fefefeefa11f
lds <lifeslong@gmail.com>
2021-05-01 21:40:35 +0200

    testsadded

--- Algorithms/LeetCode/Trees/543.DiameterBTree.cs
+++ Algorithms/LeetCode/Trees/543.DiameterBTree.cs
@@ -1,4 +1,3 @@
-using System;
 /*
 543. Diameter of Binary Tree
 https://leetcode.com/problems/diameter-of-binary-tree/
@@ -19,13 +18,13 @@ Example 1:
 
 Input: root = [1,2,3,4,5]
 Output: 3
-Explanation: 3is the length of the path [4,2,1,3] or [5,2,1,3].
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
 
 Example 2:
                     1
                    /
                   2
-                    
+
 Input: root = [1,2]
 Output: 1
 
@@ -35,12 +34,16 @@ Constraints:
     -100 <= Node.val <= 100
 */
 
+using System;
+using Xunit;
+
 namespace Algorithms.LeetCode.Trees
 {
     public class DiameterBTree {
         private int max;
         public int DiameterOfBinaryTree(TreeNode root) 
-        {        
+        { 
+            max = Int32.MinValue;       
             ITT(root);        
             return max;
         }
@@ -58,6 +61,85 @@ namespace Algorithms.LeetCode.Trees
             return 0;
         }    
 
-        //TODO: Write a test wrapper for the method
+        [Fact]
+        public void TestWrap()
+        {
+            /*
+                        1
+                       / \
+                      2   3
+                         / \
+                        4   5
+            */
+            var s = "1,2,X,X,3,4,X,X,5";
+            var obj = new Codec();
+            var root = obj.Deserialize(s);
+            
+            var res = DiameterOfBinaryTree(root);
+            Assert.Equal(3,res);
+
+
+             /*
+                        1
+                       / \
+                      2   3
+                     / \   \
+                    4   5   6
+            */
+            
+            s = "1,2,4,X,X,5,X,X,3,X,6";
+            obj = new Codec();
+            root = obj.Deserialize(s);
+            
+            res = DiameterOfBinaryTree(root);
+            Assert.Equal(4,res);
+
+            /*
+                        1
+                       / \
+                      3   2
+                     / 
+                    5   
+            */
+            
+            s = "1,3,5,X,X,X,2";
+            obj = new Codec();
+            root = obj.Deserialize(s);
+            
+            res = DiameterOfBinaryTree(root);
+            Assert.Equal(3,res);
+
+            /*
+                        1
+                       /
+                      3   
+                     / 
+                    5   
+            */
+            
+            s = "1,3,5,X,X,X";
+            obj = new Codec();
+            root = obj.Deserialize(s);
+            
+            res = DiameterOfBinaryTree(root);
+            Assert.Equal(2,res);
+
+            /*
+                        1
+                       / \
+                      3   2 
+                     /     \
+                    5       6
+                   /         \
+                  7           8  
+            */
+            
+            s = "1,3,5,7,X,X,X,X,2,X,6,X,8";
+            obj = new Codec();
+            root = obj.Deserialize(s);
+            
+            res = DiameterOfBinaryTree(root);
+            Assert.Equal(6,res);
+        }
     }
-}
+}  
