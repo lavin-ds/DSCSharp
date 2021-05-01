58baa56b1de557c657076ebea770d8734e510693
lds <lifeslong@gmail.com>
2021-05-01 13:33:13 +0200

    DEscription updated

--- Algorithms/LeetCode/Trees/543.DiameterBTree.cs
+++ Algorithms/LeetCode/Trees/543.DiameterBTree.cs
@@ -11,13 +11,21 @@ The diameter of a binary tree is the length of the longest path between any two 
 The length of a path between two nodes is represented by the number of edges between them.
 
 Example 1:
+                    1
+                   / \
+                  2   3
+                 / \
+                4   5
 
 Input: root = [1,2,3,4,5]
 Output: 3
 Explanation: 3is the length of the path [4,2,1,3] or [5,2,1,3].
 
 Example 2:
-
+                    1
+                   /
+                  2
+                    
 Input: root = [1,2]
 Output: 1
 
@@ -25,8 +33,6 @@ Constraints:
 
     The number of nodes in the tree is in the range [1, 104].
     -100 <= Node.val <= 100
-
-
 */
 
 namespace Algorithms.LeetCode.Trees
