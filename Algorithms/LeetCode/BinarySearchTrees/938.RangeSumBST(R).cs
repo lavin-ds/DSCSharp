b721d21c6cc491a1ac30cd4a2cf7b6460aa07a9f
lds <lavin.github@outlook.com>
2023-01-06 07:07:56 +0100

    Fixed logic for range sum

--- Algorithms/LeetCode/BinarySearchTrees/938.RangeSumBST(R).cs
+++ Algorithms/LeetCode/BinarySearchTrees/938.RangeSumBST(R).cs
@@ -3,8 +3,8 @@
 https://leetcode.com/problems/range-sum-of-bst/
 Easy
 
-Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
-
+Given the root node of a binary search tree and two integers low and high, 
+return the sum of values of all nodes with a value in the inclusive range [low, high].
 Example 1:
                     10
                    /  \
@@ -14,6 +14,7 @@ Example 1:
 
 Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
 Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
 
 Example 2:
                      10
@@ -26,6 +27,7 @@ Example 2:
               
 Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
 Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
 
 Constraints:
 
@@ -61,11 +63,8 @@ namespace Algorithms.LeetCode.BinarySearchTrees
                 sum = sum + root.val;
             }
             
-            if(root.val>low)
-                RangeSum(root.left);
-            if(root.val<high)
-                RangeSum(root.right);
-            
+            RangeSum(root.left);
+            RangeSum(root.right);
         }
 
         [Fact]
@@ -74,9 +73,19 @@ namespace Algorithms.LeetCode.BinarySearchTrees
             var s = "1,2,X,X,3,4,X,X,5";
             var obj = new Codec();
             var root = obj.Deserialize(s);
-
             var res = RangeSumBST(root,2,4);
             Assert.Equal(9,res);
+       }
+
+        [Fact]
+        public void TestWrap2()
+        {
+            var s = "10,5,3,X,X,7,X,X,15,X,18";
+            var obj = new Codec();
+            var root = obj.Deserialize(s);
+
+            var res = RangeSumBST(root,7,15);
+            Assert.Equal(32,res);
        }
     }
 }
